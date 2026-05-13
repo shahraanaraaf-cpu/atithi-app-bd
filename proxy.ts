72001032d4a1ee2629405064b321a5656ce91375
shahraanaraaf-cpu <shahraan.araaf@gmail.com>
2026-05-13 20:20:59 +0100

    Add files via upload

--- proxy.ts
+++ proxy.ts
@@ -1,26 +1,13 @@
-import createIntlMiddleware from 'next-intl/middleware';
-import { NextRequest, NextResponse } from 'next/server';
+import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
-import { updateSession } from './utils/supabase/middleware';
 
-const intlMiddleware = createIntlMiddleware(routing);
-
-export default async function proxy(req: NextRequest) {
-  // Redirect /search to /en/search
-  if (req.nextUrl.pathname === '/search') {
-    const url = req.nextUrl.clone()
-    url.pathname = '/en/search'
-    return NextResponse.redirect(url)
-  }
-
-  // First run the next-intl middleware to get the localized response
-  const res = intlMiddleware(req);
-  
-  // Then pass that response through Supabase to handle auth tokens
-  return await updateSession(req, res);
-}
+export default createMiddleware(routing);
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(bn|en)/:path*', '/search']
+  // Match all pathnames except for
+  // - API routes
+  // - _next (Next.js internals)
+  // - _vercel (Vercel internals)
+  // - Files with extensions (e.g. favicon.ico)
+  matcher: ['/', '/(bn|en)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
 };
